210cefc451a47f94c67ca2217a3cdfbad197b562
bmarvin <borismarvin@yandex.ru>
2024-03-07 00:04:58 +0300

    added compress adn decompress

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -62,40 +62,92 @@ type ContentTypes struct {
 	code ContentType
 }
 
-// GzipMiddleware обрабатывает запросы и ответы, добавляя сжатие gzip к содержимому.
-func GzipMiddleware(next http.Handler) http.Handler {
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ow := w
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		acceptEncoding := r.Header.Values("Accept-Encoding")
-		for _, encoding := range acceptEncoding {
-			if strings.Contains(encoding, "gzip") {
-
-				gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-				if err != nil {
-					io.WriteString(w, err.Error())
-					return
-				}
-				defer gz.Close()
-				w.Header().Set("Content-Encoding", "gzip")
-				next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
-				return
-			} else {
-				next.ServeHTTP(w, r)
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			cw := newCompressWriter(w)
+			ow = cw
+			defer cw.Close()
+		}
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
+			r.Body = cr
+			defer cr.Close()
 		}
-	})
+
+		h.ServeHTTP(ow, r)
+	}
 }
 
-type gzipWriter struct {
-	http.ResponseWriter
-	Writer io.Writer
+type compressWriter struct {
+	w  http.ResponseWriter
+	zw *gzip.Writer
 }
 
-func (w gzipWriter) Write(b []byte) (int, error) {
-	// w.Writer будет отвечать за gzip-сжатие, поэтому пишем в него
-	return w.Writer.Write(b)
+func newCompressWriter(w http.ResponseWriter) *compressWriter {
+	return &compressWriter{
+		w:  w,
+		zw: gzip.NewWriter(w),
+	}
 }
+
+func (c *compressWriter) Header() http.Header {
+	return c.w.Header()
+}
+
+func (c *compressWriter) Write(p []byte) (int, error) {
+	return c.zw.Write(p)
+}
+
+func (c *compressWriter) WriteHeader(statusCode int) {
+	if statusCode < 300 {
+		c.w.Header().Set("Content-Encoding", "gzip")
+	}
+	c.w.WriteHeader(statusCode)
+}
+
+func (c *compressWriter) Close() error {
+	return c.zw.Close()
+}
+
+type compressReader struct {
+	r  io.ReadCloser
+	zr *gzip.Reader
+}
+
+func newCompressReader(r io.ReadCloser) (*compressReader, error) {
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &compressReader{
+		r:  r,
+		zr: zr,
+	}, nil
+}
+
+func (c compressReader) Read(p []byte) (n int, err error) {
+	return c.zr.Read(p)
+}
+
+func (c *compressReader) Close() error {
+	if err := c.r.Close(); err != nil {
+		return err
+	}
+	return c.zr.Close()
+}
+
 func InitializeConfig(startAddr string, baseAddr string) config.Args {
 	envStartAddr := os.Getenv("SERVER_ADDRESS")
 	envBaseAddr := os.Getenv("BASE_ADDRESS")
